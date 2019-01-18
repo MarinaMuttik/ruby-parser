6a8b8e9cd013c223bd4ea1faccd1c1b499205981
MMuttik <mmuttik@live.co.uk>
2019-01-18 19:09:46 +0000

    added split weblog method with failing test

--- lib/viewer.rb
+++ lib/viewer.rb
@@ -1,9 +1,18 @@
 class Viewer
+  def initialize(log)
+    @log = File.read(log)
+    @views = {}
+  end
+
   def split_ipv4(log)
     ip = log.scan(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/).join
   end
 
   def split_webpage(log)
     webpage = log.scan(/\b(\/[\w]+{1,})/).join
+  end
+
+  def split_weblog
+    'fail'
   end
 end
