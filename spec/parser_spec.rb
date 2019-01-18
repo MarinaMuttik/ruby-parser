ad0c79b6236a6607f917e4e0c5e806e958f39928
MMuttik <mmuttik@live.co.uk>
2019-01-18 19:44:01 +0000

    corrected test for unique counts

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,7 +7,7 @@ describe Parser do
     it "Returns a hash of webpages with unique views count" do
       parser_test.count_unique_views.should ==
         { "/help_page/1"=>3,
-          "/contact"=>2,
+          "/contact"=>1,
           "/home"=>3,
           "/about/2"=>1,
           "/about"=>1 }
