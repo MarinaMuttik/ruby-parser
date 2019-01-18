f0340b28c08a7cb19c41d12fcef48acd679bf62f
MMuttik <mmuttik@live.co.uk>
2019-01-18 18:20:59 +0000

    updated test to correct desrciption of test

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -4,7 +4,7 @@ describe Viewer do
   let(:log) { Viewer.new }
 
   describe '.split_ipv4' do
-    it "Returns a valid IP of 4 numbers (<12 characters) seperated by 3 periods'" do
+    it "Returns a valid IP of 4 sets of numbers (<12 characters) seperated by 3 periods'" do
       log.split_ipv4('111.111.111.111/about').should == '111.111.111.111'
     end
   end
