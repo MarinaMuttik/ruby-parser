36788d954771ebd9d190d050df2b16021e286635
MMuttik <mmuttik@live.co.uk>
2019-01-18 22:51:17 +0000

    updated rspec .should to .to eq()

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,34 +5,37 @@ describe Parser do
 
   describe '.count_unique_views' do
     it "Returns a hash of webpages with unique views count" do
-      parser_test.count_unique_views.should ==
+      expect(parser_test.count_unique_views).to eq (
         { "/help_page/1"=>3,
           "/contact"=>1,
           "/home"=>3,
           "/about/2"=>1,
           "/about"=>1 }
+        )
     end
   end
 
   describe '.count_total_views' do
     it "Returns a hash of webpages with total views count" do
-      parser_test.count_total_views.should ==
+      expect(parser_test.count_total_views).to eq (
         { "/help_page/1"=>3,
           "/contact"=>2,
           "/home"=>3,
           "/about/2"=>1,
           "/about"=>1 }
+        )
     end
   end
 
   describe '.webpage_views' do
     it "Returns a hash of webpages with hashes of unique and total view counts" do
-      parser_test.webpage_views.should ==
+      expect(parser_test.webpage_views).to eq (
       { "/help_page/1"=>{"Unique views"=>3, "Total views"=>3},
         "/contact"=>{"Unique views"=>1, "Total views"=>2},
         "/home"=>{"Unique views"=>3, "Total views"=>3},
         "/about/2"=>{"Unique views"=>1, "Total views"=>1},
         "/about"=>{"Unique views"=>1, "Total views"=>1} }
+      )
     end
   end
 end
