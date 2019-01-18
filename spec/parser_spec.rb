16d751eca4416f2f86b728d17e26e92943544fc8
MMuttik <mmuttik@live.co.uk>
2019-01-18 19:06:46 +0000

    added new test for weblog split

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,6 +2,7 @@ require 'viewer'
 
 describe Viewer do
   let(:log) { Viewer.new }
+  let(:weblogfile) { Viewer.new('/test.log') }
 
   describe '.split_ipv4' do
     it "Returns a valid IP of 4 sets of numbers (<12 characters) seperated by 3 periods" do
@@ -13,6 +14,17 @@ describe Viewer do
     it "Returns a webpage route" do
       log.split_webpage('111.111.111.111/about').should == '/about'
       log.split_webpage('111.111.111.111/about/2').should == '/about/2'
+    end
+  end
+
+  describe '.split_weblog' do
+    it "Returns a hash of webpages with ip views" do
+      weblogfile.split_weblog.should ==
+        { "/help_page/1"=>["126.318.035.038", "929.398.951.889", "543.910.244.929"],
+          "/contact"=>["184.123.665.067", "184.123.665.067"],
+          "/home"=>["184.123.665.067", "235.313.352.950", "316.433.849.805"],
+          "/about/2"=>["444.701.448.104"],
+          "/about"=>["061.945.150.735"] }
     end
   end
 end
